048408b10a9763f5e214d988c06b8d22f09d2159
Eric Willisson <ericwillisson@gmail.com>
2013-05-14 21:51:19 -0400

    All growers shrink, can be grown

--- flourish.js
+++ flourish.js
@@ -3,31 +3,56 @@ var main_loop;
 
 var NUM_GROWERS = 10;
 var CANVAS_MARGIN = 10;
+var GROWER_START_SIZE = 50;
 
 var growers = [];
 
 function Grower (x, y) {
     this.x = x;
     this.y = y;
-    this.size = 5;
+    this.size = GROWER_START_SIZE;
     this.growing = false;
+    this.dead = false;
 }
 Grower.prototype.draw = function (ctx) {
+    if (this.dead) {
+	return;
+    }
     ctx.beginPath ();
     ctx.fillStyle = "rgb(255, 255, 255)";
     ctx.arc (this.x, this.y, this.size, 0, Math.PI * 2, false);
     ctx.fill();
 };
 Grower.prototype.update = function () {
+    if (this.dead) {
+	return;
+    }
     if (this.growing) {
 	this.size += .5;
+    } else {
+	this.size -= .1;
+    }
+
+    if (this.size <= 0) {
+	this.dead = true;
     }
 };
 
 function generate_growers () {
     for (var i = 0; i < NUM_GROWERS; i++) {
-	var x = Math.floor(Math.random() * (canvas.width - CANVAS_MARGIN * 2) + CANVAS_MARGIN);
-	var y = Math.floor(Math.random() * (canvas.height - CANVAS_MARGIN * 2) + CANVAS_MARGIN);
+	var redo = false;
+	var x = Math.floor(Math.random() * (canvas.width - (CANVAS_MARGIN + GROWER_START_SIZE) * 2) + CANVAS_MARGIN + GROWER_START_SIZE);
+	var y = Math.floor(Math.random() * (canvas.height - (CANVAS_MARGIN + GROWER_START_SIZE) * 2) + CANVAS_MARGIN + GROWER_START_SIZE);
+	for (g in growers) {
+	    if (hypot (x - growers[g].x, y - growers[g].y) < GROWER_START_SIZE * 2) {
+		redo = true;
+		break;
+	    }
+	}
+	if (redo) {
+	    i--;
+	    continue;
+	}
 	growers.push (new Grower (x, y));
     }
 }
@@ -58,6 +83,10 @@ function motion (evt) {
     var y = evt.offsetY - 5;
 
     for (g in growers) {
+	if (growers[g].dead) {
+	    continue;
+	}
+
 	if (hypot (x - growers[g].x, y - growers[g].y) < growers[g].size) {
 	    growers[g].growing = true;
 	} else {
